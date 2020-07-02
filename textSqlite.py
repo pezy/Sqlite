f6b95afa71cdb24730d5031e6bf74c3886965dae
chenpeizhe <peizhe.chen@horizon.ai>
2020-07-02 23:22:41 +0800

    fix create table missing

--- textSqlite.py
+++ textSqlite.py
@@ -9,10 +9,11 @@ import sqlite3
 conn = sqlite3.connect("text.db")  # 连接数据
 print("成功打开数据库")
 c = conn.cursor()  # 获取游标
+c.execute('''create table if not exists company (id integer primary key asc, name text, age integer, address text, salary integer)''')
 
 sql = '''
-    insert into company (id,name,age,address,salary)
-     values (1,"张三",32,"成都",2000);
+    insert into company (name,age,address,salary)
+     values ("张三",32,"成都",2000);
 '''
 
 c.execute(sql)  # 执行sql语句
